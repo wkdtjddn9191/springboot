c5779c4d889bd81c31a14692c6b5f12a53dc7da3
root <root@ip-10-0-10-161.ap-northeast-2.compute.internal>
2023-11-23 05:34:26 +0000

    test1

--- src/main/java/com/skills/cloud/CloudSkillsSpringBootExamApplication.java
+++ src/main/java/com/skills/cloud/CloudSkillsSpringBootExamApplication.java
@@ -17,7 +17,7 @@ public class CloudSkillsSpringBootExamApplication {
 	public HealthChecker index() {
 		
 		HealthChecker healthChecker = new HealthChecker();
-		healthChecker.setStatus("okkkkttttttttttSSSSSSSSSSSSSSSSSSSStk");
+		healthChecker.setStatus("okttpppp");
 		
 		return healthChecker;
 	}
