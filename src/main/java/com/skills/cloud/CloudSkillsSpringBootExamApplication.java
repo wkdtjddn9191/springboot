a9c33a1a00f0ca18f6f0c56872da324a02827bd6
Ubuntu <ubuntu@ip-10-0-10-161.ap-northeast-2.compute.internal>
2023-11-23 06:02:50 +0000

    test2

--- src/main/java/com/skills/cloud/CloudSkillsSpringBootExamApplication.java
+++ src/main/java/com/skills/cloud/CloudSkillsSpringBootExamApplication.java
@@ -17,7 +17,7 @@ public class CloudSkillsSpringBootExamApplication {
 	public HealthChecker index() {
 		
 		HealthChecker healthChecker = new HealthChecker();
-		healthChecker.setStatus("success test11");
+		healthChecker.setStatus("success test12");
 		
 		return healthChecker;
 	}
